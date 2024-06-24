b370ce8708304937c2dd95ca9734278cfcac8e7c
d4nieldev <danielohayon2016@gmail.com>
2024-06-24 09:57:32 +0000

    Task 1 - created channel data structure and boilerplate for the api

--- kernel/channel.c
+++ kernel/channel.c
@@ -1,4 +1,24 @@
 #include "channel.h"
-#include "params.h"
+#include "param.h"
 
 struct channel channel[NCHANNEL];
+
+int
+channel_create(void) {
+    return -1;
+}
+
+int
+channel_put(int cd, int data) {
+    return -1;
+}
+
+int
+channel_take(int cd, int *data) {
+    return -1;
+}
+
+int
+channel_destroy(int cd) {
+    return -1;
+}
--- kernel/channel.h
+++ kernel/channel.h
@@ -1,3 +1,12 @@
 struct channel {
-    
-}
+    struct spinlock lock;  // lock in case two processes try to access the channel
+
+    struct proc *parent;   // Parent process
+
+    int valid;             // if non-zero, channel contains data
+    int data;              // the data
+    int state;             // if non-zero, channel is open for communication
+
+    // bonus
+    // int refcount;         // the number of processes using this channel
+};
