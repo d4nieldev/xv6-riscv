fcd2615db104ab16fa4176debcd25e847fff84b4
d4nieldev <danielohayon2016@gmail.com>
2024-06-24 14:33:12 +0000

    enhanced channel test to include prints

--- user/channel_test.c
+++ user/channel_test.c
@@ -10,23 +10,47 @@ int main() {
     }
     if (fork() == 0) {
         if (channel_put(cd, 42) < 0) {
-            printf("Failed to put data in channel\n");
+            printf("Failed to put 42 in channel\n");
             exit(1);
+        } else {
+            printf("Put 42 in channel\n");
         }
-        channel_put(cd, 43); // Sleeps until cleared
-        // Handle error
-        channel_destroy(cd);
-        // Handle error
+        if (channel_put(cd, 43) < 0) {
+            printf("Failed to put 43 in channel\n");
+            exit(1);
+        } else {
+            printf("Put 43 in channel\n");
+        }
+
+        if (channel_destroy(cd) < 0) {
+            printf("Failed to destroy channel\n");
+            exit(1);
+        } else {
+            printf("Channel was destroyed successfully\n");
+        }
     } else {
         int data;
-        if (channel_take(cd, &data) < 0) { // 42
+        if (channel_take(cd, &data) < 0) {
             printf("Failed to take data from channel\n");
             exit(1);
+        } else {
+            printf("Took data=%d from channel\n", data); // 42
         }
-        data = channel_take(cd, &data); // 43
-        // Handle error
-        data = channel_take(cd, &data); // Sleep until child destroys channel
-        // Handle error
+
+        if (channel_take(cd, &data) < 0) { // 43
+            printf("Failed to take data from channel\n");
+            exit(1);
+        } else {
+            printf("Took data=%d from channel\n", data); // 43
+        }
+        
+        if (channel_take(cd, &data) < 0) {
+            // Sleep until child destroys channel
+            printf("Failed to take data from channel (ok)\n");
+        } else {
+            printf("Took data from channel (error)\n");
+            exit(1);
+        }
     }
     exit(0);
 }
